17ebaa12bfa5d2698e903d00458743f5c1a031a1
Jael <jael.ruvalcaba@uabc.edu.mx>
2023-02-27 11:10:46 -0800

    Initial Commit

---
+++ TMDBCoppelTestUITests/TMDBCoppelTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TMDBCoppelTestUITestsLaunchTests.swift
+//  TMDBCoppelTestUITests
+//
+//  Created by Jael  on 27/02/23.
+//
+
+import XCTest
+
+final class TMDBCoppelTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
